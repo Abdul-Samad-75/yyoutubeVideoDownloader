1a1fe6f32477d82190eda908d2a265137be7464e
Abdul-Samad-75 <abdulsamad7523@gmail.com>
2025-01-09 23:55:27 +0530

    done

--- server.js
+++ server.js
@@ -44,42 +44,7 @@ app.get('/video-info', async (req, res) => {
     }
 });
 
-// app.get('/video-info', async (req, res) => {
-//     try {
-//         const videoURL = req.query.url;
-//         console.log("Received video URL:", videoURL);
 
-//         if (!videoURL || !ytdl.validateURL(videoURL)) {
-//             return res.status(400).json({ error: "Invalid YouTube URL provided." });
-//         }
-
-//         const info = await ytdl.getInfo(videoURL);
-
-//         // Group formats by quality label
-//         const groupedFormats = info.formats.reduce((acc, format) => {
-//             if (format.qualityLabel) {
-//                 if (!acc[format.qualityLabel]) {
-//                     acc[format.qualityLabel] = [];
-//                 }
-//                 acc[format.qualityLabel].push({
-//                     itag: format.itag,
-//                     mimeType: format.mimeType,
-//                     hasVideo: format.hasVideo || false,
-//                     hasAudio: format.hasAudio || false,
-//                 });
-//             }
-//             return acc;
-//         }, {});
-
-//         res.json({
-//             title: info.videoDetails.title,
-//             qualities: groupedFormats, // Grouped formats by quality
-//         });
-//     } catch (error) {
-//         console.error("Error fetching video info:", error.message);
-//         res.status(500).json({ error: "Failed to fetch video information. Please try again later." });
-//     }
-// });
 
 app.get('/download', async (req, res) => {
     try {
